c439f056769df1ee60b0f4200180b463470402d8
joaomoraesdev <jmoraes2003@hotmail.com>
2025-06-24 16:08:29 -0300

    alteração baseURL teste localhost

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // Cria uma instância do Axios com a URL base da API
 const api = axios.create({
-  baseURL: "https://pack-and-promote.azurewebsites.net/", // URL base da API
+  //baseURL: "https://pack-and-promote.azurewebsites.net/", // URL base da API
   // baseURL: "https://api.com.br/", // URL base da API
+  baseURL: "http://localhost:5119"
 });
 
 export default api;
