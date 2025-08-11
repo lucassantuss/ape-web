8b4fc0ab05208b3aa271be6db8dbadbe87feb751
Lucas Araujo dos Santos <lucassantuss@outlook.com>
2025-08-11 00:30:20 -0300

    CORS

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Cria uma instância do Axios com a URL base da API
 const api = axios.create({
-  baseURL: "https://ape-api.azurewebsites.net", // Ambiente de Produção
+  baseURL: '/api',
+  // baseURL: "https://ape-api.azurewebsites.net", // Ambiente de Produção
   // baseURL: "https://ape-dev.azurewebsites.net", // Ambiente de Desenvolvimento
   // baseURL: "http://localhost:5000" // Local
 });
