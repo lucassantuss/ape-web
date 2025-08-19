a92e423972a270739c516cb2429a9b900b144e77
Lucas Araujo dos Santos <lucassantuss@outlook.com>
2025-08-18 21:44:24 -0300

    upd MinhaConta

--- src/hooks/useMinhaConta.js
+++ src/hooks/useMinhaConta.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../services/api";
 import { formatCPF, validateCPF } from "utils/Validations";
-import useAuthentication from "hooks/useAuthentication";
+import { useAuthentication } from "context/Authentication";
 
 export default function useMinhaConta() {
   const [editando, setEditando] = useState(false);
